fd68a31431306633d8f1678258f9e432901346e4
Paul O’Mahony <paul@atyos.io>
2024-11-11 22:25:11 +0100

    add image normization

--- ml/train.py
+++ ml/train.py
@@ -1,4 +1,4 @@
-from torchvision.transforms import ToTensor
+from torchvision.transforms import ToTensor, Compose, Normalize
 from torch.utils.data import DataLoader
 import torch.nn as nn
 import torch.optim as optim
@@ -21,7 +21,7 @@ def train_model(model, dataloaders, criterion, optimizer, num_epochs, device):
     with Live() as live:
         live.log_param('num_epochs', num_epochs)
         live.log_param('learning_rate', LEARNING_RATE)
-        live.log_param('trasformations', None)
+        live.log_param('trasformations', 'Normalize')
         for epoch in range(num_epochs):
             print(f"Epoch {epoch + 1}/{num_epochs}")
             print("-" * 20)
@@ -78,7 +78,12 @@ if __name__ == '__main__':
     test_images_paths = list(TEST_IMAGES_PATH.iterdir())
     random.shuffle(test_images_paths)
     
-    train_set = CatsAnDogs(train_images_paths[:2000], transforms=ToTensor())
+    transforms = Compose([
+        ToTensor(),
+        Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+    ])
+    
+    train_set = CatsAnDogs(train_images_paths[:20000], transforms=ToTensor())
     test_set = CatsAnDogs(test_images_paths[:5000], transforms=ToTensor())
     
     train_loader = DataLoader(train_set, batch_size=64, shuffle=True)
